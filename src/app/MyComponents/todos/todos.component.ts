67f47e8fb3eaac0bf3e1e788533fb7639cb2e868
Muzammil Ismail <muzammilismail8@gmail.com>
2024-10-17 17:53:46 +0500

    adding validation

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -10,28 +10,30 @@ export class TodosComponent implements OnInit {
   todos: Todo[];
 
   constructor() {
-    // this.todos = [
-    //   {
-    //     sno: 1,
-    //     title: 'This is Title 1',
-    //     desc: 'Description 1',
-    //     isActive: true,
-    //   },
-    //   {
-    //     sno: 2,
-    //     title: 'This is Title 2',
-    //     desc: 'Description 2',
-    //     isActive: true,
-    //   },
-    //   {
-    //     sno: 3,
-    //     title: 'This is Title 3',
-    //     desc: 'Description 3',
-    //     isActive: true,
-    //   },
-    // ];
+    let localStorageTotos = localStorage.getItem('todos');
 
-    this.todos = JSON.parse(localStorage.getItem('todos'));
+    if (localStorageTotos == null) {
+      this.todos = [
+        {
+          sno: 1,
+          title: 'This is Title 1',
+          desc: 'Description 1',
+          isActive: true,
+        },
+        {
+          sno: 2,
+          title: 'This is Title 2',
+          desc: 'Description 2',
+          isActive: true,
+        },
+        {
+          sno: 3,
+          title: 'This is Title 3',
+          desc: 'Description 3',
+          isActive: true,
+        },
+      ];
+    } else this.todos = JSON.parse(localStorage.getItem('todos'));
   }
 
   onDeleteClick(todo: Todo) {
